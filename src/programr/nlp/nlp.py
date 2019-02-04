d2c790ddfa64c9fb672d059bdb2a54fb335e62a8
Rohola <hilbert.cantor@gmail.com>
2019-02-04 14:57:51 -0700

    nltk.data.path now contains the nltk_data_dir

--- src/programr/nlp/nlp.py
+++ src/programr/nlp/nlp.py
@@ -5,7 +5,7 @@ from programr.nlp.semantic.semantic_similarity import SemanticSimilarity
 from programr.nlp.semantic.sentiment_analysis import SentimentAnalysis
 from programr.utils.logging.ylogger import YLogger
 import nltk #todo remove dependency on nltk when user wants to use only spacy
-
+import os
 
 class NLP(object):
 
@@ -33,7 +33,11 @@ class NLP(object):
         return self._sentiment_analysis
 
     def _load_libraries(self, nltk_data_dir):
-        if nltk_data_dir:
+        if nltk_data_dir and os.path.isdir(nltk_data_dir):
+
+            if nltk_data_dir not in nltk.data.path:
+                nltk.data.path.append(nltk_data_dir)
+
             try:
                 nltk.download("popular", download_dir=nltk_data_dir, quiet=True)
             except Exception as excp:
